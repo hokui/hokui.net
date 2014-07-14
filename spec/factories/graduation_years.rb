7f1c5faa11cf40620e5f31ba365160d6b1cdbbd3
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-07-14 11:55:22 +0900

    insert model annotation

--- spec/factories/graduation_years.rb
+++ spec/factories/graduation_years.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: graduation_years
+#
+#  id         :integer          not null, primary key
+#  year       :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,33 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                              :integer          not null, primary key
+#  email                           :string(255)      not null
+#  crypted_password                :string(255)      not null
+#  salt                            :string(255)      not null
+#  created_at                      :datetime
+#  updated_at                      :datetime
+#  activation_state                :string(255)
+#  activation_token                :string(255)
+#  activation_token_expires_at     :datetime
+#  approval_state                  :string(255)
+#  reset_password_token            :string(255)
+#  reset_password_token_expires_at :datetime
+#  reset_password_email_sent_at    :datetime
+#  last_login_at                   :datetime
+#  last_logout_at                  :datetime
+#  last_activity_at                :datetime
+#  last_login_from_ip_address      :string(255)
+#  first_name                      :string(255)      default(""), not null
+#  given_name                      :string(255)      default(""), not null
+#  handle_name                     :string(255)      default(""), not null
+#  birthday                        :date             not null
+#  email_mobile                    :string(255)
+#  admin                           :boolean          default(FALSE), not null
+#  graduation_year_id              :integer          default(1), not null
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
