3cc12a7c88b97c039db239026ee9721b3e2f904c
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-07-18 11:44:31 +0900

    AccessToken functions
    ref: https://github.com/NoamB/sorcery/pull/415

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -11,5 +11,35 @@
 #
 
 class AccessToken < ActiveRecord::Base
+  TERM_OF_VALIDITY = 2.weeks
+
   belongs_to :user
+
+  validates(:user_id) { presence }
+  validates(:token)   { presence; uniqueness }
+
+  before_create :generate_token, :update_last_activity_time
+
+  def self.delete_expired
+    self
+      .where
+      .not(last_activity_at: (Time.now - TERM_OF_VALIDITY)..Time.now)
+      .delete_all
+  end
+
+  def expired?
+    Time.now - TERM_OF_VALIDITY > self.last_activity_at
+  end
+
+  def update_last_activity_time
+    self.last_activity_at = Time.now
+  end
+
+  private
+
+  def generate_token
+    begin
+      self.token = SecureRandom.hex(32)
+    end while self.class.find_by(token: self.token)
+  end
 end
