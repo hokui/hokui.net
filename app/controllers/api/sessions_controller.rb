e167a397f10c97ca70d2ebd46b5c751b7a71cbad
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-07-21 10:17:58 +0900

    sessions#create

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Api::SessionsController < Api::ApplicationController
+  def create
+    json_hash = JSON.parse(request.body.read)
+    email    = json_hash["email"]
+    password = json_hash["password"]
+
+    user = User.authenticate(email, password)
+    if user
+      access_token = AccessToken.create(user: user)
+      render json: { logged_in: true, token: access_token.token }, status: 201
+    else
+      render json: { logged_in: false }, status: 422
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, only: :create
+    resources :sessions, only: :create
+    resources :users,    only: :create
   end
 
   get "/users/activate/:email_local/:activation_token" => "users#activate", as: :user_activation
