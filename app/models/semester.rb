05ae3632a502db772b20f3ac01d7475054d24731
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-10-23 13:04:14 +0900

    validate format of Semester#identifier

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -13,5 +13,5 @@ class Semester < ActiveRecord::Base
   belongs_to :class_year
   has_and_belongs_to_many :subjects
 
-  validates(:identifier) { presence; uniqueness(scope: :class_year_id) }
+  validates(:identifier) { presence; uniqueness(scope: :class_year_id); format(with: /[1-6][ab]/) }
 end
