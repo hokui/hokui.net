d173253933a5a39a30c8c56c0cc94142179027ad
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-12-13 12:19:07 +0900

    check current password when updating password

--- app/controllers/api/passwords_controller.rb
+++ app/controllers/api/passwords_controller.rb
@@ -2,6 +2,10 @@ class Api::PasswordsController < Api::ApplicationController
   class BlankPasswordError < StandardError; end
 
   def update
+    unless User.authenticate(@current_user.email, params[:current_password])
+      render json: { errors: { current_password: ["is wrong"] } }, status: 401
+      return
+    end
     @current_user.change_password!(password_params[:password])
     render json: @current_user, status: 200
   rescue BlankPasswordError
@@ -14,6 +18,7 @@ class Api::PasswordsController < Api::ApplicationController
   def password_params
     params = ActionController::Parameters.new(JSON.parse(request.body.read)).
       permit(
+        :current_password,
         :password
       )
     if params[:password].blank?
--- spec/requests/passwords_spec.rb
+++ spec/requests/passwords_spec.rb
@@ -4,20 +4,29 @@ RSpec.describe "Passwords", :type => :request do
   describe "GET /api/profile/password" do
     it "updates password", autodoc: true do
       guest = create_guest_with_token
-      patch_with_token(guest, "/api/profile/password", { password: "hogefuga" }.to_json)
+      patch_with_token(guest, "/api/profile/password", { current_password: "guest", password: "hogefuga" }.to_json)
       expect(response.status).to eq(200)
       expect(User.authenticate(guest.email, "hogefuga")).to eq(guest)
     end
 
+    it "returns 401 if current password is wrong" do
+      guest = create_guest_with_token
+      patch_with_token(guest, "/api/profile/password", { current_password: "admin", password: "hogefuga" }.to_json)
+      expect(response.status).to eq(401)
+      expect(json["errors"]["current_password"]).to include("is wrong")
+      expect(User.authenticate(guest.email, "")).to be_nil
+    end
+
     it "returns 422 if new password is blank" do
       guest = create_guest_with_token
-      patch_with_token(guest, "/api/profile/password", { password: "" }.to_json)
+      patch_with_token(guest, "/api/profile/password", { current_password: "guest", password: "" }.to_json)
       expect(response.status).to eq(422)
       expect(json["errors"]["password"]).to include("can't be blank")
+      expect(User.authenticate(guest.email, "")).to be_nil
     end
 
     it "returns 401 to an unauthorized client" do
-      patch("/api/profile/password", { password: "hogefuga" }.to_json)
+      patch("/api/profile/password", { current_password: "guest", password: "hogefuga" }.to_json)
       expect(response.status).to eq(401)
     end
   end
