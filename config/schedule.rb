5492cb88aa48af6b16a95a34b26b874499007dc2
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-12-24 11:17:21 +0900

    send approval request once a day

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@ env :PATH, ENV['PATH']
 
 set :output, "log/cron.log"
 
+every 1.day, at: "9:00 am" do
+  runner "User.send_approval_request_to_admin!"
+end
+
 every :saturday, at: "3am" do
   rake "access_token:delete_expired_tokens"
 end
