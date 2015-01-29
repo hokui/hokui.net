31b38aafee53ba199780b16f62b79405fb3711d2
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-01-29 12:55:51 +0900

    annotate

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: news
+#
+#  id         :integer          not null, primary key
+#  text       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  title      :string
+#
+
 class News < ActiveRecord::Base
   validates(:text) { presence }
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,14 +3,15 @@
 # Table name: subjects
 #
 #  id         :integer          not null, primary key
-#  title_ja   :string(255)      not null
-#  title_en   :string(255)      not null
+#  title_ja   :string           not null
+#  title_en   :string           not null
 #  created_at :datetime
 #  updated_at :datetime
 #
 
 class Subject < ActiveRecord::Base
   has_and_belongs_to_many :semesters
+  has_many :documents
 
   validates(:title_ja) { presence; uniqueness }
   validates(:title_en) { presence; uniqueness; format(with: /\A[0-9a-z_]+\Z/) }
