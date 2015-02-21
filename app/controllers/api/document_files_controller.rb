01148d45e58ae0bb778c673db87a5eef4dd40ebf
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-02-21 12:07:43 +0900

    spec for POST /api/document_files

--- app/controllers/api/document_files_controller.rb
+++ app/controllers/api/document_files_controller.rb
@@ -2,8 +2,13 @@ class Api::DocumentFilesController < Api::ApplicationController
   before_action :set_document_file, only: :download_token
 
   def create
-    document = Document.where(document_params).first_or_create!
-    @document_file = document.document_files.new(document_file_params).attach_file(file_params)
+    # NOTE this action returns either Document or DocumentFile
+    @document = Document.where(document_params).first_or_initialize
+    unless @document.save
+      render json: @document, status: 422
+      return
+    end
+    @document_file = @document.document_files.new(document_file_params).attach_file(file_params)
     if @document_file.save_with_file
       render json: @document_file, status: 201
     else
---
+++ spec/requests/api/document_files_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe "DocumentFiles", type: :request do
+  before do
+    @guest = create_guest_with_token
+  end
+
+  describe "POST /api/document_files" do
+    before do
+      @filepath = File.join(Rails.root, "misc", "uploaded", "000001-dummy.pdf")
+      @json_params = {
+        subject_id: 1,
+        class_year: 93,
+        code: 1,
+        user_id: @guest.id,
+        comments: "hoge"
+      }
+    end
+
+    it "creates an AR record and saves uploaded file" do
+      old_size = DocumentFile.count
+      post_with_token(
+        @guest,
+        "/api/document_files",
+        file: fixture_file_upload(@filepath, "application/pdf"),
+        json: @json_params.to_json
+      )
+      expect(response.status).to eq(201)
+      expect(DocumentFile.count).to eq(old_size + 1)
+      df_id = json["id"]
+      expect(File.read("/tmp/#{sprintf("%06d", df_id)}-000001-dummy.pdf")).to eq(File.read(@filepath))
+    end
+
+    it "returns 422 if uploaded file is missing" do
+      pending "can't handle unexpected error"
+      fail
+      # TODO: following code raises ActionDispatch::ParamsParser::ParseError
+      # post_with_token(
+      #   @guest,
+      #   "/api/document_files",
+      #   json: @json_params.to_json
+      # )
+      # expect(response.status).to eq(422)
+    end
+
+    it "returns 422 with invalid params" do
+      @json_params.delete(:code)
+      old_size = DocumentFile.count
+      post_with_token(
+        @guest,
+        "/api/document_files",
+        file: fixture_file_upload(@filepath, "application/pdf"),
+        json: @json_params.to_json
+      )
+      expect(response.status).to eq(422)
+      expect(DocumentFile.count).to eq(old_size)
+    end
+
+    it "returns 401 to an unauthorized client" do
+      old_size = DocumentFile.count
+      post(
+        "/api/document_files",
+        file: fixture_file_upload(@filepath, "application/pdf"),
+        json: @json_params.to_json
+      )
+      expect(response.status).to eq(401)
+      expect(DocumentFile.count).to eq(old_size)
+    end
+  end
+end
