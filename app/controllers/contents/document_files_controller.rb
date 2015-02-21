24c0cb6f53c398e9db679fee8957febfb0d8c7c7
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-02-21 17:09:31 +0900

    add testcase

--- app/controllers/contents/document_files_controller.rb
+++ app/controllers/contents/document_files_controller.rb
@@ -1,28 +1,25 @@
 class Contents::DocumentFilesController < ActionController::Base
-  before_action :set_document_file, only: :show
-
   def show
-    if @document_file
-      if authorize_download
-        filepath = @document_file.file_fullpath
-        filename = ERB::Util.url_encode(@document_file.file_name)
-        filesize = File.stat(filepath).size
-        send_file(filepath, filename: filename, length: filesize)
-      else
-        # TODO
-        render text: "Download not authorized", status: 401
-      end
-    else
+    begin
+      @document_file = DocumentFile.find(params[:id])
+    rescue
       # TODO
       render text: "File not found", status: 404
+      return
+    end
+
+    if authorize_download
+      filepath = @document_file.file_fullpath
+      filename = ERB::Util.url_encode(@document_file.file_name)
+      filesize = File.stat(filepath).size
+      send_file(filepath, filename: filename, length: filesize)
+    else
+      # TODO
+      render text: "Download not authorized", status: 401
     end
   end
 
   private
-
-  def set_document_file
-    @document_file = DocumentFile.find(params[:id])
-  end
 
   def authorize_download
     download_token = DownloadToken.find_token(params[:download_token])
