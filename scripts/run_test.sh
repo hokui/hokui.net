ef74bd3d77e466168e83db3bb446a398aa2c988f
endaaman <buhibuhidog@gmail.com>
2015-05-08 23:29:16 +0900

    update repo token

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,6 +2,5 @@ bundle exec rspec
 npm test
 # bash scripts/e2e_on_sauce.sh
 
-COVERALLS_SERVICE_NAME=codeship
-COVERALLS_REPO_TOKEN=GlzGIL1scY32eexp5V2Q8QDkrPnBci22e
-cat ./build-report/lcov.info | ./node_modules/coveralls/bin/coveralls.js;
+export COVERALLS_REPO_TOKEN=GWDKAQhpqOcb4urILg0JZze6TUdykiRbN
+cat ./build-report/lcov.info | ./node_modules/coveralls/bin/coveralls.js
