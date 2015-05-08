cfb8bc5f2faa294f06763913093c9eae9f6d9f51
endaaman <buhibuhidog@gmail.com>
2015-05-08 20:09:31 +0900

    add coveralls env

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,4 +2,6 @@ bundle exec rspec
 npm test
 # bash scripts/e2e_on_sauce.sh
 
+COVERALLS_SERVICE_NAME=codeship
+COVERALLS_REPO_TOKEN=GlzGIL1scY32eexp5V2Q8QDkrPnBci22e
 cat ./build-report/lcov.info | ./node_modules/coveralls/bin/coveralls.js;
